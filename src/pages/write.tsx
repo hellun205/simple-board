22ea418ef361272484d6e7660f81cbdce0d49fe7
hellun205 <hellun515@gmail.com>
2023-08-04 20:50:50 +0900

    write style

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,5 +1,9 @@
 import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import classNames from "classnames/bind";
+import styles from "../styles/write.module.css";
+
+const cx = classNames.bind(styles);
 
 interface Props {
   writePost: (title: string, content: string, author: string) => void;
@@ -13,28 +17,33 @@ const Write: FC<Props> = ({ writePost }) => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <input
-        placeholder="작성자"
-        value={author}
-        onChange={(e) => {
-          setAuthor(e.target.value);
-        }}
-      ></input>
-      <input
-        placeholder="제목"
-        value={title}
-        onChange={(e) => {
-          setTitle(e.target.value);
-        }}
-      ></input>
-      <input
+    <div className={cx("wrapper")}>
+      <div>
+        <input
+          className={cx("author")}
+          placeholder="작성자"
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        ></input>
+        <input
+          className={cx("title")}
+          placeholder="제목"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        ></input>
+      </div>
+
+      <textarea
         placeholder="내용"
         value={content}
         onChange={(e) => {
           setContent(e.target.value);
         }}
-      ></input>
+      ></textarea>
       <button
         onClick={() => {
           writePost(title, content, author);
